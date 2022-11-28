85a1b74c5ed3a90e7f5b8f55ae89f1e82c6faddf
David Gomez Pacheco <dgdband@gmail.com>
2022-11-28 13:11:55 -0600

    [ADD] Agregando validadores para que traiga un valor de interfaz IPv4 sin importar que contenga diferentes interfaces creadas

---
+++ README.md
@@ -0,0 +1,9 @@
+# Desktop Interfaces
+
+<h5>Progrmas para producción</h5>
+<p>El programa para instalar de manera global en NodeJS es <a href="https://www.npmjs.com/package/pkg">pkg</a> </p>
+<br/>
+<p>Y el comando para ejecutarlo es: </p>
+<blockquote>
+    pkg [nombre_archivo_app].js -t node[version_nodejs]-win-x64 
+</blockquote>
--- services/DesktopService.js
+++ services/DesktopService.js
@@ -2,16 +2,30 @@ const os = require('os');
 
 class DesktopService {
 
+    /**
+     * Método que obtiene la información de la MAC, IP y hostname del cliente
+     * @returns response con información de la máquina cliente
+     */
     getNetworkInterfaces() {
         
         try {
             const networkInterfaces = os.networkInterfaces();
+            const regexInterfaces = new RegExp(/^(Ethernet|LAN)([\s][0-9])?/)
 
-            const ethnt = networkInterfaces.Ethernet;
+            const interfaces = Object.keys(networkInterfaces).filter(inter => regexInterfaces.test(inter))
 
-            const mac = [... new Set(ethnt.map(item => item.mac))];
-            const ip = [... new Set(ethnt.filter(item => item.family === "IPv4").map(item => item.address))];
-
+            // * Obtengo una lista de los datos de Ethernet o LAN y filtro que tengan el atributo family y address
+            // * y también verifico que la familia sea IPv4
+            const ethntOrLan = interfaces.map(inter => {
+                const interFound = networkInterfaces[inter].filter(item => item.family && item.address && item.family === "IPv4")[0]
+                return interFound
+            })
+            
+            // * Genero una lista de macs no repetibles
+            const mac = [... new Set(ethntOrLan.map(item => item.mac))];
+            // * Genero una lista de ip no repetibles
+            const ip = [... new Set(ethntOrLan.filter(item => item.family === "IPv4").map(item => item.address))];
+            
             const macWithoutColon = mac[0].toUpperCase().replace(/:/g, '');
 
             const response = {
