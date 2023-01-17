026151ac689447554888c9dff47543c30559b56f
David Gomez Pacheco <dgdband@gmail.com>
2023-01-17 12:50:53 -0600

    [ADD] Generando el merge y ajustando scripts para mantener el comando de compilacion de win 7

--- services/DesktopService.js
+++ services/DesktopService.js
@@ -23,31 +23,13 @@ class DesktopService {
         
         try {
             const networkInterfaces = os.networkInterfaces();
-            //const regexInterfaces = new RegExp(/^(Ethernet|LAN)[\s]?[0-9]?/)
-
-            //const interfaces = Object.keys(networkInterfaces).filter(inter => regexInterfaces.test(inter))
 
             // * Obtengo una lista de los datos de Ethernet o LAN y filtro que tengan el atributo family y address
             // * y también verifico que la familia sea IPv4
-            // const ethntOrLan = interfaces.map(inter => {
-            //     const interFound = networkInterfaces[inter].filter(item => item.family && item.address && item.family === "IPv4" && item.address === ipAddress)[0]
-            //     return interFound
-            // })
-            
-            // const ethntOrLan = Object.keys(networkInterfaces).filter(key => {
-            //     const ethn = networkInterfaces[key].find(inter => {
-            //         return inter.address && inter.family && inter.address === ipAddress && inter.family === 'IPv4'
-            //     })
-
-
-            //     return ethn
-            // })
 
             let ethntOrLan;
-
             for(const key of Object.keys(networkInterfaces)){
-                const ethn  = networkInterfaces[key].filter(inter => {
-                    // console.log(inter.address)
+                const ethn  = networkInterfaces[key].filter(inter => {                    
                     return inter.address && inter.family && inter.address == ipAddress && inter.family == 'IPv4'
                 })
 
@@ -56,16 +38,11 @@ class DesktopService {
                 }
 
             }
-           
-
-            console.log(ethntOrLan)
             
             // * Genero una lista de macs no repetibles
-            // const mac = [... new Set(ethntOrLan.map(item => item.mac))];
             const mac = ethntOrLan.mac
 
             // * Genero una lista de ip no repetibles
-            // const ip = [... new Set(ethntOrLan.filter(item => item.family === "IPv4").map(item => item.address))];
             const ip = ethntOrLan.address
             
             const macWithoutColon = mac.toUpperCase().replace(/:/g, '');
